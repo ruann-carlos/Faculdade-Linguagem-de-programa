f1d762416d2ac6f97133352f3481831ba4b4d0f6
ruann_carlos <ruann.carlos@indb.com.br>
2020-09-08 11:00:41 -0300

    exercicio 04 finalizado

---
+++ exercicios08-09/exercicio03Aula/index.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+    document.getElementById("calcular").onclick = calcular;
+}
+function calcular(){
+    let f1 = 0, f2 = 0, f3 = 0, f4 = 0, f5 = 0; //faixas de idade (1 até 5)
+    let idade, i;
+
+    while(i <8){
+        idade = Number(prompt("Informe uma idade"));
+
+        if(idade <= 15){
+            f1++;
+        }else if (idade <=30){
+            f2++;
+        }else if(idade <= 45){
+            f3++;
+        }else if (idade <= 60){
+            f4++
+        }else{
+            f5++
+        }
+        console.log(`faixa 1 ${f1}, faixa 2 ${f2}, faixa 3 ${f3},faixa 4 ${f4},faixa 5 ${f5}`);
+        console.log(`% da faixa 1 ${f1/8*100} % da faixa 5 ${f5/8*100}`)
+    }    
+}
